838a59de5f831c82ce6b5ac64e2ca662ee70891f
Ankit <iankit17@gmail.com>
2020-08-13 15:46:11 +0530

    add block-BJaabr

---
+++ block-BJaabr/code/index.js
@@ -0,0 +1,182 @@
+// Look at the example below and do the same for other problems.
+
+
+/*
+Write a function named convertToString that accepts a number and converts the number into a string (hint: use String()) and return the converted value.
+
+Do the following for the given problem:
+*/
+
+//1. Write a Function Decleration
+function convertToString(n) {
+  return String(n);
+}
+
+//2. Write a Function Expression
+let convertToString = function (n) {
+  return String(n);
+};
+
+//3. Write an Arrow Function
+let convertToString = (n) => String(n);
+
+//4. Write an Arrow Function with curly brackets (if possible)
+let convertToString = (n) => {
+  return String(n);
+};
+
+//5. Execute the function
+convertToString(21);
+
+//6. Execute the function and store the return value in a variable.
+let returnValue = convertToString(23);
+
+//7. What is the typeof returnValue
+// typeof returnValue is "string"
+
+
+
+/* 1. Add one to the given value
+
+Write a function named `addOne` that accepts a number, adds one to the given value and`return` it(hint: use return keyword).
+
+Do the following with above problem.
+*/
+
+
+// 1. Write a Function Declaration
+
+// 2. Write a Function Expression
+
+// 3. Write an Arrow Function
+
+// 4. Write an Arrow Function with curly brackets(if possible)
+
+// 5. Execute the function
+
+// 6. Execute the function and store the return value in a variable.
+
+// 7. What is the typeof returnValue
+
+
+
+/* 2. Subtract one
+
+Write a function named `substractOne` that accepts a number, substracts one from the given value and return it. (hint: use return keyword)
+
+Do the following with above problem.
+*/
+
+// 1. Write a Function Declaration
+
+// 2. Write a Function Expression
+
+// 3. Write an Arrow Function
+
+// 4. Write an Arrow Function with curly brackets(if possible)
+
+// 5. Execute the function
+
+// 6. Execute the function and store the return value in a variable.
+
+// 7. What is the typeof returnValue
+
+
+/* 3. Add two numbers
+
+Write a function named `sum` that accepts two numbers and returns the sum of them.
+
+Do the following with above problem.
+*/
+
+// 1. Write a Function Declaration
+
+// 2. Write a Function Expression
+
+// 3. Write an Arrow Function
+
+// 4. Write an Arrow Function with curly brackets(if possible)
+
+// 5. Execute the function
+
+// 6. Execute the function and store the return value in a variable
+
+// 7. What is the typeof returnValue
+
+
+
+/* 4. Square of the given value
+
+Write a function named `square` that accepts a number and returns the square of the number.
+
+Do the following with above problem.
+*/
+
+// 1. Write a Function Declaration
+
+// 2. Write a Function Expression
+
+// 3. Write an Arrow Function
+
+// 4. Write an Arrow Function with curly brackets(if possible)
+
+// 5. Execute the function
+
+// 6. Execute the function and store the return value in a variable.
+
+// 7. What is the typeof returnValue
+
+
+
+
+/* 5. Find isGreater
+
+Write a function named isGreater that accepts two numbers`x` and`y` and returns`true` if x is greater than y or return `false`.
+
+Do the following with above problem.
+*/
+
+// 1. Write a Function Declaration
+
+// 2. Write a Function Expression
+
+// 3. Write an Arrow Function
+
+// 4. Write an Arrow Function with curly brackets(if possible)
+
+// 5. Execute the function
+
+// 6. Execute the function and store the return value in a variable.
+
+// 7. What is the typeof returnValue
+
+
+
+
+/* 6.
+
+Write a function named `oddOrEven` that accepts a`number` and returns
+
+  - `Number is odd` for odd number
+  - `Number is even` for even number.
+
+Do the following with above problem.
+*/
+
+// 1. Write a Function Declaration
+
+// 2. Write an anonymous Function Expression
+
+// 3. Write an named Function Expression
+
+// 4. Write an Arrow Function
+
+// 5. Write an Arrow Function with curly brackets(if possible)
+
+// 6. Execute the function
+
+// 7. Execute the function and store the return value in a variable.
+
+// 8. What is the typeof returnValue
+
+
