83b7cfcc84c74ba88fd94da13f2869503ed681da
Ankit <iankit17@gmail.com>
2020-08-13 23:07:28 +0530

    fix: the spelling mistakes

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -5,26 +5,26 @@
 /*
 2. Create a function named `fullName` with the following steps:
 
-  - Prompts a message saying`Enter your first name` and store the value into variable named`firstName`
-  - Prompts a message saying`Enter your last name` and store the value into variable named`lastName`
-  - Concat the value of fistName and lastName and store it in another variable named`fullName`.
-  - Alert`fullName`
+  - Prompts a message saying `Enter your first name` and store the value into variable named `firstName`
+  - Prompts a message saying `Enter your last name` and store the value into variable named `lastName`
+  - Concat the value of fistName and lastName and store it in another variable named `fullName`.
+  - Alert `fullName`
 */
 
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
 
-  - Prompts a message saying`Enter the first number` and store the value into variable named`firstNum`
-  - Prompts a message saying`Enter the second` and store the value into variable named`secondNum`
-  - Concat the value of fistName and lastName and store it in another variable named`sum`.
-  - Alert`sum`
+  - Prompts a message saying `Enter the first number` and store the value into variable named `firstNum`
+  - Prompts a message saying `Enter the second` and store the value into variable named `secondNum`
+  - Add the value of firstNum and secondNum and store it in another variable named `sum`.
+  - Alert `sum`
 */
 
 /*
 4. Create a function named `getTable` with the following steps:
 
-  - Prompts a message saying`Enter a number` and store the value into variable named`num`
-  - Using`console.log` logs the Multiplication Table of the number entered by the user.
+  - Prompts a message saying `Enter a number` and store the value into variable named `num`
+  - Using `console.log` logs the Multiplication Table of the number entered by the user.
   - Example: Number is 9
 
   9 * 1 = 9
@@ -39,19 +39,17 @@
   9 * 10 = 90
 */
 
-
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
-  - Prompts a message saying`Enter a year` and store the value into variable named`year`
-  - Alert`[year] is a leap year` otherwise`[year] is not a leap year`
+  - Prompts a message saying `Enter a year` and store the value into variable named`year`
+  - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
-
 
 /*
 6. Create a function named `getFactorial` with the following steps:
 
-  - Prompts a message saying`Enter a number` and store the value into variable named`number`
-  - Alert`The factorial of [number] is [factorial]`
+  - Prompts a message saying `Enter a number` and store the value into variable named`number`
+  - Alert `The factorial of [number] is [factorial]`
 
 */
